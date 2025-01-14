ea9d1ef0a5e73d9718d78864cfe871f87b2f2b57
inocente91 <per24070@byui.edu>
2025-01-13 19:54:36 -0800

    got stuck in this part, will try to re read the lection again and compare my code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,6 +6,61 @@ class Program
     {
         Console.Write( "Enter your grade percentage: ");
         int percentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "":
+
+        if (percentage >= 90)
+        {
+            letterr = "A";
         
+        }
+
+        elif (percentage >= 80)
+        {
+            letter = "B"
+        }
+
+        elif (percentage >= 70)
+
+        {
+            letter = "C";
+        }
+
+        elif (percentage > = 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter ="F";
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass.")
+        }
+
+        if (letter != "F")
+        {
+            int lastDigit = percentage % 10;
+            if (lastDigit > 7)
+            {
+                sign = "+";
+            }
+        }
+
+        elif (lastDigit <3)
+        {
+            sign = "-";
+        }
+
+
     }
 }
