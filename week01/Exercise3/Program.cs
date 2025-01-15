a2711f0926806b29a53b171e5b36900243beb945
inocente91 <per24070@byui.edu>
2025-01-14 22:32:17 -0800

    I decided to add the strecth challenge but I got stuck so I have to look for my notes from python to add a new while to ask the player if want to play again or no

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,30 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
+        string playAgain = "yes"; 
 
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
-        int guess = -1; 
-
-
-        while (guess != magicNumber)
+        while (playAgain.ToLower() == "yes")
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-            if (guess > magicNumber)
+            int guess = -1; 
+            int attempts = 0; 
+
+            Console.WriteLine("Guess the magic number between 1 and 100!");
+
+            
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++; 
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {attempts} guesses.");
+                }
             }
-            else if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
         }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
