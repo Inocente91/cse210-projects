9f82a81e35bca79f3d7209690a17e5235061836b
inocente91 <per24070@byui.edu>
2025-01-14 21:18:32 -0800

    I started coding while loops but got stuck in this part so I need to review a few things before proceed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        
+                
+
     }
 }
