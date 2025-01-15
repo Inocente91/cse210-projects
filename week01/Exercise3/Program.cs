7683a65931304007a59ff09abf23cb00c195d84c
inocente91 <per24070@byui.edu>
2025-01-14 21:32:51 -0800

    So after I review the assigment I was able to find that we need to copy and paste the Random Generator code, since the user will be guessing a number I decided to choose the while loop so the program loop till the user gues what is the magic number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,9 +6,29 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
-        
+        int magicNumber = int.Parse(Console.ReadLine());  
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < guess)
+            {
                 
+            }
+        }
+
 
     }
 }
