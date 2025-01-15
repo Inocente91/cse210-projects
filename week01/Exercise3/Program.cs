89801a1e96127542208486950342e2818ff53e08
inocente91 <per24070@byui.edu>
2025-01-14 22:19:25 -0800

    Step 3 finnaly done, I have to correct my code in the while part cause I have inverted the magin number < for the feedback

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,35 +1,33 @@
 using System;
-using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {  
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+    {
 
-       int guess = 0;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1; 
 
 
-       while (guess != magicNumber)
-       {
+        while (guess != magicNumber)
+        {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
-            if (guess < magicNumber)
+            if (guess > magicNumber)
             {
-                 Console.WriteLine("Higher");
+                Console.WriteLine("Lower");
             }
-
-            else if (guess > magicNumber)
-             {
-                 Console.WriteLine("Lower");
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
             }
-
             else
             {
-                  Console.WriteLine("You guessed it!");
+                Console.WriteLine("You guessed it!");
             }
-       }
+        }
     }
 }
