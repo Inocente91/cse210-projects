071163705b779a967e23045799dfb8c675bbe155
inocente91 <per24070@byui.edu>
2025-01-14 22:10:15 -0800

    Finnally I was able to do the 2 part of the asigment to loop till the user guess the #

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,30 +4,32 @@ using System.Security.Cryptography;
 class Program
 {
     static void Main(string[] args)
-    {  // Here start the part 1 for the guessing game:
-
+    {  
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
-
-        if (guess < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-
-        else 
-        {
-            Console.WriteLine("You guessed it");
-        }
+       int guess = 0;
 
 
+       while (guess != magicNumber)
+       {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
 
+            if (guess < magicNumber)
+            {
+                 Console.WriteLine("Higher");
+            }
+
+            else if (guess > magicNumber)
+             {
+                 Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                  Console.WriteLine("You guessed it!");
+            }
+       }
     }
 }
