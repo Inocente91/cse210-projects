2b861c9579ffb93878a1142abdaefbcd02e0206a
inocente91 <per24070@byui.edu>
2025-01-29 13:44:47 -0800

    Initialize project and add base structure

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        List<Scripture> scriptures = LoadScriptures("scriptures.txt");
+        if (scriptures.Count == 0)
+        {
+            Console.WriteLine("No scriptures found in the file.");
+            return;
+        }
+
+    Random random = new Random();
+    Scripture selectedScripture = scriptures[random.Next(scriptures.Count)];
+
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine(selectedScripture.GetDisplayText());
+        if (selectedScripture.IsCompletelyHidden())
+            break;
+    }   
     }
-}
