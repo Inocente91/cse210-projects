267a131ad4d3f479526b5616cc48f13ccb66379c
inocente91 <per24070@byui.edu>
2025-01-29 11:34:57 -0800

    Create Journal class with list of Entry objects

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> _entries;
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
+    
+}
