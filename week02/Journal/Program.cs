21b21f9a2c3c39b06f94c6d8160366300151b43d
inocente91 <per24070@byui.edu>
2025-01-29 12:30:04 -0800

    Implemented SaveToFile() using StreamWriter, create LoadFromFile() with StreamReader, added random prompt selection from list, added store entries with current date, standardize date fromat (yyyy-MM-dd) and include input for prompts filenames

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -10,7 +10,7 @@ public class Program
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
         "If I had one thing I could do over today, what would it be?"
-    }:
+    };
 
     public static void Main(string[] args)
     {
@@ -23,14 +23,54 @@ public class Program
             Console.WriteLine(" Exit");
             Console. Write("Choose an option: ");
             string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    WriteNewEntry();
+                    break;
+                case "2":
+                    _journal.DisplayEntries();
+                    break;
+                case "3":
+                    SaveJournal();
+                    break;
+                case "4":
+                    LoadJournal();
+                    break:
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");    
+                    break;            
+
+            }
         }
     }
-
-
-
-
-
-
-
-
+    
+    private static void WriteNewEntry()
+    {
+        Random random = new Random();
+        string prompt = _prompts[random.Next(_prompts.Count)];
+        Console.WriteLine($"Promt: {prompt}");
+        Console.Write("Your response: ");
+        string response = Console.ReadLine();
+        string date = DateTime.Now.ToString("yyyy-MM-dd");
+        _journal.AddEntry(new Entry(prompt, response, date));
+        Console.WriteLine("Entry added successfully.");
+    }
+    
+    private static void SaveJournal()
+    {
+        Console.Write("Enter filename to save: ");
+        string filename = Console.ReadLine();
+        _journal.SaveToFile(filename);
+    }
+    
+    private static void LoadJournal()
+    {
+        Console.Write("Enter filename to load: ");
+        string filename = Console.ReadLine();
+        _journal.LoadFromFile(filename);
+    }
 }
