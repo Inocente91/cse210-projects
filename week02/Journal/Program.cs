0ff765d4a91e3af1a75ab2bc24a5a39ba0ec82ce
inocente91 <per24070@byui.edu>
2025-01-29 12:07:39 -0800

    Create program.cs, added menu loop with options 1-5, added prompt list with 5 default questions and included exit option

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,7 +1,30 @@
 using System;
 
-class Program
+public class Program
 {
+    private static Journal _journal = new Journal();
+    private static List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best par of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    }:
+
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the Journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("Load the jorunal from a file");
+            Console.WriteLine(" Exit");
+            Console. Write("Choose an option: ");
+            string choice = Console.ReadLine();
+        }
+    }
 
 
 
