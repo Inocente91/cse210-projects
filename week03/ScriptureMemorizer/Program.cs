53b78e114b35f63a9b6e5cbe91ab9603c2d643f9
inocente91 <per24070@byui.edu>
2025-02-07 20:18:56 -0800

    Finishe the program.cs for the scripture memorizerd I added the options for the user so he will decide to play or quit

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,44 +1,57 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
-class Program
+namespace ScriptureMemorizer
 {
-    static void Main(string[] args)
+    class Program
     {
-        List<Scripture> scriptures = LoadScriptures("scriptures.txt");
-        if (scriptures.Count == 0)
+        static void Main(string[] args)
         {
-            Console.WriteLine("No scriptures found in the file.");
-            return;
+            Console.WriteLine("Welcome to the Scripture Memorizer!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+
+            ScriptureLibrary library = new ScriptureLibrary("scriptures.txt");
+            Scripture scripture = library.GetRandomScripture();
+
+
+            if (scripture == null)
+            {
+                Console.WriteLine("\n No scriptures found. Try adding some to 'scriptures.txt'.");
+                return;
+            }
+
+
+            int round = 1;
+            while (true)
+            {
+                scripture.Display();
+
+
+                if (scripture.AllWordsHidden())
+                {
+                    Console.WriteLine("\n You memorized it! Well done!");
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                    break;
+                }
+
+
+                Console.Write("\nPress Enter to hide words or type 'quit' to exit: ");
+                string input = Console.ReadLine().ToLower();
+
+
+                if (input == "quit")
+                {
+                    Console.WriteLine("\n Goodbye! Thanks for playing.");
+                    break;
+                }
+
+
+                scripture.HideRandomWords(round < 3 ? 2 : 3);
+                round++;
+            }
         }
-
-        Random random = new Random();
-        Scripture selectedScripture = scriptures[random.Next(scriptures.Count)];
-
-        while (true)
-         {
-            Console.Clear();
-            Console.WriteLine(selectedScripture.GetDisplayText());
-            if (selectedScripture.IsCompletelyHidden())
-                break;
-
-            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit:");
-            string input = Console.ReadLine();
-            if (input.ToLower() == "quit")
-                break;
-
-            selectedScripture.HideRandomWords(3);        
-         }   
     }
-
-    static List<Scripture> LoadScriptures(string filePath)
-    {
-        List<Scripture> scriptures = new List<Scripture>();
-        if (!File.Exists(filePath))
-            return scriptures;
-    }
-
 }
 
