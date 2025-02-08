cecbee1fb867746a6a92e1ccf94a892a94900a2f
inocente91 <per24070@byui.edu>
2025-02-07 20:32:33 -0800

    Added ScriptureWord class to manage word hiding in scripture

---
+++ week03/ScriptureMemorizer/ScriptureWord.cs
@@ -0,0 +1,11 @@
+namespace ScriptureMemorizer
+{
+    public class ScriptureWord
+    {
+        private string _text;
+        private bool _isHidden;
+
+
+        public ScriptureWord(string text)
+    }
+}
