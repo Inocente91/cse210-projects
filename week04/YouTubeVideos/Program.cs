5c3708c178f1300406541e5082b9f6d59ca80f9d
inocente91 <per24070@byui.edu>
2025-02-12 14:29:20 -0800

    I started the program creating a video object and adding comments to the video

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Video video1 = new Video("Leanr C# Basics", "Inocente Perez", 240);
+
+
+        video1.AddComment(new Comment("Alice", "This was super helpfulls!"));
+        video1.AddComment(new Comment("Bob", "Nice explanation!"));
+        video1.AddComment(new Comment("Charlie", "Great job, keep it up!"));
     }
 }
