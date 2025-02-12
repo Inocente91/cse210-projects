148cb716c80911b2ed0adf0807b799f76e5712f3
inocente91 <per24070@byui.edu>
2025-02-12 14:55:51 -0800

    executed and take an screenshot of the program running added jpg file

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        Video video1 = new Video("Leanr C# Basics", "Inocente Perez", 240);
+
+        Video video1 = new Video("Learn C# Basics", "Inocente Perez", 240);
 
 
-        video1.AddComment(new Comment("Alice", "This was super helpfulls!"));
+        video1.AddComment(new Comment("Alice", "This was super helpful!"));
         video1.AddComment(new Comment("Bob", "Nice explanation!"));
         video1.AddComment(new Comment("Charlie", "Great job, keep it up!"));
 
@@ -20,42 +22,49 @@ class Video
 {
     public string title;
     public string author;
-    public int Length;
+    public int length;
     private List<Comment> comments;
 
-
-    public Video(string title, string author, int length);
+    public Video(string title, string author, int length)
     {
-        this.title = tittle;
+        this.title = title;
         this.author = author;
-        this.length = lenght;
+        this.length = length;
         comments = new List<Comment>();
     }
 
-
-public void AddComment(Comment c)
-{
-    comments.Add(c);
-}
-
-
-public int GetCommentCount()
-{
-    return comments.Count;
-}
-
-
-public void ShowDetails()
-{
-    Console.WriteLine("Title: " + title);
-    Console.WriteLine("Author: " + author);
-    Console.WriteLine("Length: " + length);
-    Console.WriteLine("Comment: " + GetCommentCount());
-
-
-    foreach (var c in comments)
+    public void AddComment(Comment c)
     {
-        console.WriteLine("- " + c.user + ": " + c.text);
+        comments.Add(c);
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Length: " + length + " sec");
+        Console.WriteLine("Comments: " + GetCommentCount());
+
+        foreach (var c in comments)
+        {
+            Console.WriteLine("- " + c.user + ": " + c.text);
+        }
     }
 }
+
+class Comment
+{
+    public string user;
+    public string text;
+
+    public Comment(string user, string text)
+    {
+        this.user = user;
+        this.text = text;
+    }
 }
