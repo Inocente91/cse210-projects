dfeee0f483eaddb97e4ee0a806ba222c85008d5f
inocente91 <per24070@byui.edu>
2025-02-12 15:31:12 -0800

    added class product with public string that include name, price, quantity and get the total price

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -19,3 +19,23 @@ class Program
         order.ShowOrderDetails();
     }
 }
+
+
+class Product
+{
+    public string name;
+    public double price;
+    public int quantity;
+
+    public Product(string name, double price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalPrice()
+    {
+        return price * quantity;
+    }
+}
