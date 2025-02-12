59f4d787aec0595f62c7407f7fd5b05f65c49a00
inocente91 <per24070@byui.edu>
2025-02-12 14:31:17 -0800

    added video display info

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -10,5 +10,8 @@ class Program
         video1.AddComment(new Comment("Alice", "This was super helpfulls!"));
         video1.AddComment(new Comment("Bob", "Nice explanation!"));
         video1.AddComment(new Comment("Charlie", "Great job, keep it up!"));
+
+
+        video1.ShowDetails();
     }
 }
