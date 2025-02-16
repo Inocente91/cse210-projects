fa5e74f10ab60fe22baec8344cfdf8d7cc3fc466
inocente91 <per24070@byui.edu>
2025-02-16 08:55:24 -0800

    ChecklistGoal for multiple completions Tracks current progress out of a set goal (X/Y) Gives normal points plus a bonus when fully done Updates display to show progress

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -84,3 +84,45 @@ public class EternalGoal : Goal
         return $"[ ] {base.GetDetailsString()}";
     }
 }
+
+// ChecklistGoal class
+public class ChecklistGoal : Goal
+{
+    private int _targetCount;
+    private int _currentCount;
+    private int _bonusPoints;
+
+    public ChecklistGoal(string name, string description, int points, int targetCount, int bonusPoints, int currentCount = 0)
+        : base(name, description, points)
+    {
+        _targetCount = targetCount;
+        _bonusPoints = bonusPoints;
+        _currentCount = currentCount;
+    }
+
+    public override int RecordEvent()
+    {
+        _currentCount++;
+        int points = _points;
+        if (_currentCount == _targetCount)
+        {
+            points += _bonusPoints;
+        }
+        return points;
+    }
+
+    public override bool IsComplete()
+    {
+        return _currentCount >= _targetCount;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"[{(_currentCount >= _targetCount ? "X" : " ")}] {base.GetDetailsString()} -- Completed {_currentCount}/{_targetCount} times";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"{base.GetStringRepresentation()},{_targetCount},{_bonusPoints},{_currentCount}";
+    }
+}
