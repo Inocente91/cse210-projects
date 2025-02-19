fed2fbf718daab0170a0e82edf17906b94267d8d
inocente91 <per24070@byui.edu>
2025-02-19 14:57:37 -0800

    Created the base Activity class with shared fields and methods
    Added the Activity class with private fields for date and minutes. Made a constructor to set these fields. Added abstract methods for distance, speed, and pace that the other classes will need to implement. Also added a GetSummary method to give a summary of the activity.

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracker
 {
-    static void Main(string[] args)
+    // Base class for all activities
+    public abstract class Activity
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        private DateTime _date; // Date of the activity
+        private int _minutes;   // Duration in minutes
+
+        // Constructor to set date and minutes
+        public Activity(DateTime date, int minutes)
+        {
+            _date = date;
+            _minutes = minutes;
+        }
+
+        // Properties to get date and minutes
+        public DateTime Date => _date;
+        public int Minutes => _minutes;
+
+        // Abstract methods to calculate distance, speed, and pace
+        public abstract double GetDistance(); // Distance in miles
+        public abstract double GetSpeed();    // Speed in mph
+        public abstract double GetPace();    // Pace in minutes per mile
+
+        // Method to get a summary of the activity
+        public virtual string GetSummary()
+        {
+            return $"{Date.ToShortDateString()} {GetType().Name} ({Minutes} min) - " +
+                   $"Distance: {GetDistance():0.0} miles, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+        }
     }
-}
